dde18f4f4ff08bc63f420593627efe8e93f2105b
KorotinAV <AlexKor9203@yandex.ru>
2022-05-20 11:48:28 +0300

    New class variables

---
+++ src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { //класс
+    public static void main(String[] args) { // главный метод main, с него начинается выполнение программы
+        System.out.println("Hello World!");
+    }
+}
